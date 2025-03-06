9d0f220203ed3738b1de12109983313a988a1f07
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-03-06 16:33:02 -0700

    changed function name to initializeApp to represent initializing and logic.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // useful DOM variable
 const gameCardsWrapper = document.querySelector(".game-cards-wrapper");
 
-// extract the data from games.json and handle each game appropriately
-function handleData() {
+// app initialization; extract the data from games.json and handle each game appropriately
+function initializeApp() {
   gameCardsWrapper.innerHTML = ""; // doesn't allow overflow of games
   fetch("games.json")
     .then((response) => response.json())
@@ -24,9 +24,12 @@ function displayGameCard(game) {
             <div class="card-info">
                 <h2 class="title">${game.title}</h2>
                 <p class="genre">${game.genre}</p>
-                <div class="platforms">${game.platforms.map((gamePlatform) => {
-                  return <span class="platform">{gamePlatform}</span>;
-                })}</div>
+                <div class="platforms">${game.platforms
+                  .map(
+                    (gamePlatform) =>
+                      `<span class="platform">${gamePlatform}</span>`
+                  )
+                  .join("")}</div>
                 <p class="release-date">${game.releaseDate}</p>
                 <div class="company">
                    <span class="developer">${game.developer}</span> 
@@ -39,20 +42,23 @@ function displayGameCard(game) {
             </div>
             <div class="card-body">
                 <p class="description">${game.description}</p>
-                <div class="tags">${game.tags.map((tag) => {
-                  return <span class="tag">{tag}</span>;
-                })}</div>
+                <div class="tags">${game.tags
+                  .map((tag) => `<span class="tag">${tag}</span>`)
+                  .join("")}</div>
                 <span class="price">${game.price}</span>
                 <span class="${game.availability}">${game.availability}</span>
             </div>
-            <div class="card-reviews">${game.reviews.map((review) => {
-              return (
-                <div class="review">
-                  <span class="author">{review.author}</span>
-                  <span class="rating">{review.rating}</span>
-                  <p class="comment">{review.comment}</p>
-                </div>
-              );
-            })}</div>
+            <div class="card-reviews">${game.reviews
+              .map(
+                (review) =>
+                  `<div class="review">
+                    <span class="author">${review.author}</span>
+                    <span class="rating">${review.rating}</span>
+                    <p class="comment">${review.comment}</p>
+                  </div>`
+              )
+              .join("")}</div>
         </div>`;
 }
+
+initializeApp();
